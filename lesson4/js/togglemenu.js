cf45c540014a82c4ed95e7604fb4b003c77a69ef
sydnycutler <sydny.cutler@gmail.com>
2020-10-09 11:47:04 -0600

    yes

--- lesson4/js/togglemenu.js
+++ lesson4/js/togglemenu.js
@@ -3,4 +3,9 @@ try {
     document.getElementById('currentdate2').textContent = new Date().toLocaleDateString('en-US', options);
   } catch (e) {
     alert('Error with code or your browser does not support Locale');
-  }      
+  }  
+  function toggleMenu() {
+    document
+    .getElementsByClassName("navigation") [0]
+    .classList.toggle("responsive");
+  }    
