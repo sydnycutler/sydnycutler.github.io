41154f291846b7d4a93b1ef0191450cf0775103f
sydnycutler <sydny.cutler@gmail.com>
2020-10-09 12:09:04 -0600

    change

--- lesson4/js/togglemenu.js
+++ lesson4/js/togglemenu.js
@@ -1,6 +1,6 @@
 try {
     const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementById('currentdate2').textContent = new Date().toLocaleDateString('en-US', options);
+    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
   } catch (e) {
     alert('Error with code or your browser does not support Locale');
   }  
