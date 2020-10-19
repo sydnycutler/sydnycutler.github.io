c4ebd6fed73982c1ecf46338a84186bb5f2a481a
sydnycutler <sydny.cutler@gmail.com>
2020-10-18 22:19:41 -0600

    get date

--- lesson5/js/togglemenu.js
+++ lesson5/js/togglemenu.js
@@ -10,7 +10,7 @@ try {
     .classList.toggle("responsive");
   }
   function showhide() {
-    var d = new Date();
+    var d = new Date(5);
     var s = document.getElementById(d.getDay());
     s.style.display = (s.style.display == 'block') ? 'none' : 'block';
 }
