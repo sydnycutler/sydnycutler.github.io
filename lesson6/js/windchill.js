1dbe6ff47941b08d17dabc7d47244db8192609fe
sydnycutler <sydny.cutler@gmail.com>
2020-10-22 21:39:35 -0600

    go again

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -5,3 +5,4 @@
     
         (temp <= 50 && speed > 10) ? result = ((35.74+(0.6215 * temp)) - (35.75*Math.pow(speed,0.16))+(0.4275*temp)*(Math.pow(speed,0.16))).toFixed(2) : result = "N/A";
     document.getElementById("windChill").innerHTML = result;
+    }
