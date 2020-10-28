cafdefbfc942797a3f0a0a733b1d1ca9a9b4a754
sydnycutler <sydny.cutler@gmail.com>
2020-10-28 16:51:00 -0600

    go

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,30 +1,30 @@
-const imagesLoad = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const imgOption = {
+    threshold:1,
+    rootMargin: "0px 0px 50px 0px"
 
-const preloadImage = (image) => {
+};
+const loadImages = (image) => {
     image.setAttribute("src", image.getAttribute("data-src"));
     image.onload = () => {
         image.removeAttribute("data-src");
     };
-    };
-
-const imgOptions = {
-    threshold = 1,
-    rootMargin = "0px 0px 50px 0px",
 };
-if("IntersectionObserver" in window) {
-const imgObserver = new IntersectionObserver((items, observer) => {
-items.forEach((item) => {
-    if (!item.isIntersecting) {
-        return;
-    }
-    else {
-        preloadImage(item.target);
-        imgObserver.unobserve(item.target);
-    }
-});
-}, imgOptions);
 
-imagesLoad.forEach((img) => {
-    imgObserver.observe(img);
-});
-}
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
