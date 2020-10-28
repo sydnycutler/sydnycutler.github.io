e1b8212ba28f0d1ea4c6bb83980f8baa90555558
sydnycutler <sydny.cutler@gmail.com>
2020-10-28 16:28:58 -0600

    maybe

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -8,7 +8,7 @@ const preloadImage = (image) => {
     };
 
 const imgOptions = {
-    threshold = 0,
+    threshold = 1,
     rootMargin = "0px 0px 50px 0px",
 };
 if("IntersectionObserver" in window) {
