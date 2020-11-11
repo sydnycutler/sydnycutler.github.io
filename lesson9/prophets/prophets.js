155792d52a0fe6ffa224d5d89a6fd665a6203f53
sydnycutler <sydny.cutler@gmail.com>
2020-11-11 09:01:31 -0700

    js new

--- lesson9/prophets/prophets.js
+++ lesson9/prophets/prophets.js
@@ -14,7 +14,7 @@ for (let i = 0; i < prophets.length; i++) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let p = document.createElement('p');
-    let image = document.createElement('img');
+    let img = document.createElement('img');
 
     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
     card.appendChild(h2);
@@ -23,7 +23,8 @@ for (let i = 0; i < prophets.length; i++) {
     p.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
     card.appendChild(p);
     image.setAttribute('src', prophets[i].imageurl);
-    card.appendChild(image);
+    image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + "-" + prophets[i].order);
+    card.appendChild(img);
 
     document.querySelector('div.cards').appendChild(card);
 }
