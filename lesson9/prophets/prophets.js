e64a73e8d966f268795724e03bcc31b71b68af1a
sydnycutler <sydny.cutler@gmail.com>
2020-11-11 09:33:28 -0700

    happy

--- lesson9/prophets/prophets.js
+++ lesson9/prophets/prophets.js
@@ -5,25 +5,25 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         console.table(jsonObject);  // temporary checking for valid response and data parsing
-const prophets = jsonObject['prophets'];
+        const prophets = jsonObject['prophets'];
 
-for (let i = 0; i < prophets.length; i++) {
+        for (let i = 0; i < prophets.length; i++) {
 
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let p = document.createElement('p');
-    let img = document.createElement('img');
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let p = document.createElement('p');
+            let img = document.createElement('img');
 
-    h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-    card.appendChild(h2);
-    p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
-    card.appendChild(p);
-    p.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
-    card.appendChild(p);
-    image.setAttribute('src', prophets[i].imageurl);
-    image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + "-" + prophets[i].order);
-    card.appendChild(img);
+            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            card.appendChild(h2);
+            p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+            card.appendChild(p);
+            p.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
+            card.appendChild(p);
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + "-" + prophets[i].order);
+            card.appendChild(img);
 
-    document.querySelector('div.cards').appendChild(card);
-}
+            document.querySelector('div.cards').appendChild(card);
+        }
     });
