d82fdadabb3b398018ec2a6acc1a250956c80fe4
sydnycutler <sydny.cutler@gmail.com>
2020-11-18 12:55:52 -0700

    push

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -4,6 +4,12 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-  });
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
 
-  document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; 
+const desc = jsObject.weather[0].description;  
+document.getElementById('imagesrc').textContent = imagesrc; 
+document.getElementById('icon').setAttribute('src', imagesrc); 
+document.getElementById('icon').setAttribute('alt', desc);
+  });
