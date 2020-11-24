08309b5a44ed624fa8eb82066a042320fa567845
sydnycutler <sydny.cutler@gmail.com>
2020-11-24 13:02:58 -0700

    push

--- lesson11/js/togglemenu.js
+++ lesson11/js/togglemenu.js
@@ -1,3 +1,15 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+try {
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
+  function showhide() {
+    var d = new Date();
+    if (d.getDay() == 5) {
+      document.getElementById("banner").style.display = "block";
+    }
+  }
