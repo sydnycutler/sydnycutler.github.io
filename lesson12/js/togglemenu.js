0bc119c9f4e80da40aadec2417120003d309c326
sydnycutler <sydny.cutler@gmail.com>
2020-12-04 00:24:41 -0700

    go

---
+++ lesson12/js/togglemenu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+try {
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
